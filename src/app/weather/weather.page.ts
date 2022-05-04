7b748a64b3c5240330e5e11147cbd216c9b57d38
Sustavo <gust4v0309@gmail.com>
2022-05-03 22:17:55 -0300

    create weather page

--- src/app/weather/weather.page.ts
+++ src/app/weather/weather.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-weather',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.page.scss'],
 })
 export class WeatherPage implements OnInit {
+  cep: string;
+  city: string;
+  address: string;
+  cAverage: number;
+  teste: string;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  decimalPlace(n) {
+    return (Math.round(n * 100) / 100).toFixed(2);
+  }
+
+  goToMain(){
+    this.router.navigate(['main']);
+  };
+
+  weather() {
+    axios.get(`https://cep.awesomeapi.com.br/json/${this.cep}`)
+      .then((response) => {
+      this.city = response.data.city;
+      this.address = response.data.address;
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=3d9ef3a8b18ab3d37d245ebeee190711`)
+      .then((weatherResp) => {
+        this.cAverage = (weatherResp.data.main.temp - 273.15);
+        this.teste = weatherResp.data.weather[0].description;
+      });
+    });
+
+
   }
 
 }
